4ac910d735b81b3219bdfea1cf41207911d1d3d4
junsai <junsai0530@gmail.com>
2022-05-08 20:52:48 +0900

    first

--- firebasechat/ContentView.swift
+++ firebasechat/ContentView.swift
@@ -8,9 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView{
+                
+                VStack(spacing: 16){
+                    
+                
+                
+                Picker(selection: $isLoginMode, label: Text("dd")){
+                    Text("login")
+                        .tag(true)
+                    Text("Create Account")
+                        .tag(false)
+                }.pickerStyle(SegmentedPickerStyle())
+                    
+                    if !isLoginMode {
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                        
+                        
+                    }
+                    
+                    Group{
+                        TextField("Email", text: $email)
+                        TextField("Password", text: $password)
+                    }
+                    .autocapitalization(.none)
+                    .padding(12)
+                    .background(Color.white)
+                
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack{
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }.background(Color.blue)
+                     }
+                    
+                    
+                    
+              }.padding()
+                    
+                
+            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create Account")
+            .background(Color(.init(white: 0, alpha: 0.05))
+                .ignoresSafeArea())
+        }
     }
 }
 
