efdf15b738ad4d7f4bbe54d7ae27082c9dd71a50
junsai <junsai0530@gmail.com>
2022-05-08 20:34:06 +0900

    Initial Commit

---
+++ firebasechat/firebasechatApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebasechatApp.swift
+//  firebasechat
+//
+//  Created by 小暮準才 on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct firebasechatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
